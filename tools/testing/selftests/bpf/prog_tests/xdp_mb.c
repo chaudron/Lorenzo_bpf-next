638d097606de2a1ee4e0243f86a9ecd488c862ae
Eelco Chaudron <echaudro@redhat.com>
2020-10-02 11:09:37 +0200

    bpf: update multi-buffer selftest to include multi-buffer access
    This updates the xdp_mb selftest to include testing the new
    bpf_xdp_get_frag_count() helper.
    
    Signed-off-by: Eelco Chaudron <echaudro@redhat.com>

--- tools/testing/selftests/bpf/prog_tests/xdp_mb.c
+++ tools/testing/selftests/bpf/prog_tests/xdp_mb.c
@@ -11,7 +11,7 @@ static void test_xdp_mb_check_len(void)
 {
 	int test_sizes[] = { 128, 4096, 9000 };
 	struct test_xdp_multi_buff *pkt_skel;
-	char *pkt_in = NULL, *pkt_out = NULL;
+	__u8 *pkt_in = NULL, *pkt_out = NULL;
 	__u32 duration = 0, retval, size;
 	int err, pkt_fd, i;
 
@@ -33,13 +33,16 @@ static void test_xdp_mb_check_len(void)
 
 	/* Run test for specific set of packets */
 	for (i = 0; i < ARRAY_SIZE(test_sizes); i++) {
-		int frags_count;
+		int frags_count, j;
+
+		for (j = 0; j < test_sizes[i]; j++)
+			pkt_in[j] = j & 0xff;
 
 		/* Run test program */
 		err = bpf_prog_test_run(pkt_fd, 1, pkt_in, test_sizes[i],
 					pkt_out, &size, &retval, &duration);
 
-		if (CHECK(err || retval != XDP_PASS,
+		if (CHECK(err || retval != XDP_PASS || size != test_sizes[i],
 			  "test_run", "err %d errno %d retval %d size %d[%d]\n",
 			  err, errno, retval, size, test_sizes[i]))
 			goto out;
@@ -60,6 +63,138 @@ static void test_xdp_mb_check_len(void)
 			  pkt_skel->data->test_result_frags_len,
 			  test_sizes[i] - pkt_skel->data->test_result_xdp_len))
 			goto out;
+
+		for (j = 0; j < test_sizes[i]; j++) {
+			if (CHECK(pkt_out[j] != (j & 0xff),
+				  "content", "Packet content at offset %d is "
+				  "wrong 0x%x != 0x%x\n",
+				  j, pkt_out[j], j & 0xff))
+				break;
+		}
+	}
+out:
+	if (pkt_out)
+		free(pkt_out);
+	if (pkt_in)
+		free(pkt_in);
+
+	test_xdp_multi_buff__destroy(pkt_skel);
+}
+
+static void test_xdp_mb_access(void)
+{
+	int test_sizes[] = { 128, 1500, 4096, 9000 };
+	struct test_xdp_multi_buff *pkt_skel;
+	__u8 *pkt_in = NULL, *pkt_out = NULL;
+	__u32 duration = 0, retval, size;
+	int err, pkt_fd, i;
+
+	/* Load XDP program */
+	pkt_skel = test_xdp_multi_buff__open_and_load();
+	if (CHECK(!pkt_skel, "pkt_skel_load", "test_xdp_mb skeleton failed\n"))
+		goto out;
+
+	/* Allocate resources */
+	pkt_out = malloc(test_sizes[ARRAY_SIZE(test_sizes) - 1]);
+	pkt_in = malloc(test_sizes[ARRAY_SIZE(test_sizes) - 1]);
+	if (CHECK(!pkt_in || !pkt_out, "malloc",
+		  "Failed malloc, in = %p, out %p\n", pkt_in, pkt_out))
+		goto out;
+
+	pkt_fd = bpf_program__fd(pkt_skel->progs._xdp_mb_access);
+	if (pkt_fd < 0)
+		goto out;
+
+	/* Run test for specific set of packets */
+	for (i = 0; i < ARRAY_SIZE(test_sizes); i++) {
+		int j;
+
+		memset(pkt_in, 0, test_sizes[i]);
+
+		/* Run test program */
+		err = bpf_prog_test_run(pkt_fd, 1, pkt_in, test_sizes[i],
+					pkt_out, &size, &retval, &duration);
+
+		if (CHECK(err || retval != XDP_PASS || size != test_sizes[i],
+			  "test_run", "err %d errno %d retval %d size %d[%d]\n",
+			  err, errno, retval, size, test_sizes[i]))
+			goto out;
+
+		/* Verify test results */
+		if (CHECK(!pkt_skel->data->test_result_current_frag,
+			  "result_frag", "mb_frag test failed!\n"))
+			goto out;
+
+		for (j = 0; j < test_sizes[i]; j++) {
+			if (CHECK(pkt_out[j] != (j & 0xff),
+				  "content", "Packet content at offset %d is "
+				  "wrong 0x%x != 0x%x\n",
+				  j, pkt_out[j], j & 0xff))
+				break;
+		}
+	}
+out:
+	if (pkt_out)
+		free(pkt_out);
+	if (pkt_in)
+		free(pkt_in);
+
+	test_xdp_multi_buff__destroy(pkt_skel);
+}
+
+static void test_xdp_mb_adjust_helpers(void)
+{
+	struct test_xdp_multi_buff *pkt_skel;
+	int i, err, pkt_fd, test_size = 4096;
+	__u8 *pkt_in = NULL, *pkt_out = NULL;
+	__u32 duration = 0, retval, size;
+
+	/* Load XDP program */
+	pkt_skel = test_xdp_multi_buff__open_and_load();
+	if (CHECK(!pkt_skel, "pkt_skel_load", "test_xdp_mb skeleton failed\n"))
+		goto out;
+
+	/* Allocate resources */
+	pkt_out = malloc(test_size);
+	pkt_in = malloc(test_size);
+	if (CHECK(!pkt_in || !pkt_out, "malloc",
+		  "Failed malloc, in = %p, out %p\n", pkt_in, pkt_out))
+		goto out;
+
+	pkt_fd = bpf_program__fd(pkt_skel->progs._xdp_mb_adjust_helpers);
+	if (pkt_fd < 0)
+		goto out;
+
+	/* Select test to run */
+	memset(pkt_in, 0, test_size);
+	pkt_in[0] = 1; //TEST
+
+	/* Run test program */
+	err = bpf_prog_test_run(pkt_fd, 1, pkt_in, test_size,
+				pkt_out, &size, &retval, &duration);
+
+
+	switch (pkt_in[0]) {
+	case 0:
+		test_size -= 7;
+		break;
+	case 1:
+		test_size -= 32 - 7;
+		break;
+	}
+
+	if (CHECK(err || retval != XDP_PASS || size != test_size,
+		  "test_run", "err %d errno %d retval %d size %d[%d]\n",
+		  err, errno, retval, size, test_size))
+		goto out;
+
+	/* Verify test results */
+	for (i = 0; i < test_size; i++) {
+		if (CHECK(pkt_out[i] != (i & 0xff),
+			  "content", "Packet content at offset %d is "
+			  "wrong 0x%x != 0x%x\n",
+			  i, pkt_out[i], i & 0xff))
+			break;
 	}
 out:
 	if (pkt_out)
@@ -74,4 +209,8 @@ void test_xdp_mb(void)
 {
 	if (test__start_subtest("xdp_mb_check_len_frags"))
 		test_xdp_mb_check_len();
+	if (test__start_subtest("xdp_mb_access"))
+		test_xdp_mb_access();
+	if (test__start_subtest("xdp_mb_adjust_helpers"))
+		test_xdp_mb_adjust_helpers();
 }
--- tools/testing/selftests/bpf/progs/test_xdp_multi_buff.c
+++ tools/testing/selftests/bpf/progs/test_xdp_multi_buff.c
@@ -8,6 +8,7 @@
 __u64 test_result_frags_count = UINT64_MAX;
 __u64 test_result_frags_len = UINT64_MAX;
 __u64 test_result_xdp_len = UINT64_MAX;
+__u64 test_result_current_frag = UINT64_MAX;
 
 SEC("xdp_check_mb_len")
 int _xdp_check_mb_len(struct xdp_md *xdp)
@@ -20,5 +21,96 @@ int _xdp_check_mb_len(struct xdp_md *xdp)
 	test_result_frags_count = bpf_xdp_get_frags_count(xdp);
 	return XDP_PASS;
 }
+
+
+static __always_inline void fill_xdp_packet(struct xdp_md *xdp)
+{
+	__u8 *data_end = (void *)(long)xdp->data_end;
+	__u8 *data = (void *)(long)xdp->data;
+	int i, frag, frag_count, offset = 0;
+
+	/* Overwrite non-fragment buffer. */
+	for (i = 0; i < 4096; i++) {
+		if (data + 1 <= data_end)
+			*(data++) = i & 0xff;
+		else
+			break;
+	}
+
+	test_result_current_frag = 1;
+	if (!xdp->mb)
+		return;
+
+	/* Do the additional fragments. */
+	frag_count = bpf_xdp_get_frags_count(xdp);
+	for (frag = 1; frag <= 4; frag++) {
+
+		offset += i;
+
+		if (frag > frag_count)
+			break;
+
+		bpf_xdp_set_current_frag(xdp, frag);
+
+		if (xdp->mb_frag != frag)
+			test_result_current_frag = 0;
+
+		data_end = (unsigned char *)(long)xdp->data_end;
+		data = (unsigned char *)(long)xdp->data;
+
+		for (i = 0; i < 4096; i++) {
+			if (data + 1 <= data_end)
+				*(data++) = (i + offset) & 0xff;
+			else
+				break;
+		}
+	}
+}
+
+SEC("xdp_mb_access")
+int _xdp_mb_access(struct xdp_md *xdp)
+{
+	fill_xdp_packet(xdp);
+	return XDP_PASS;
+}
+
+
+SEC("xdp_mb_adjust_helpers")
+int _xdp_mb_adjust_helpers(struct xdp_md *xdp)
+{
+	__u8 *data_end = (void *)(long)xdp->data_end;
+	__u8 *data = (void *)(long)xdp->data;
+	__u8 test;
+
+	if (!xdp->mb)
+		return XDP_DROP;
+
+	if (data + 1 <= data_end)
+		test = data[0];
+
+	switch(test) {
+	case 0:
+		/* Make packet 7 bytes smaller by shrinking mb's head */
+		bpf_xdp_set_current_frag(xdp, 1);
+		bpf_xdp_adjust_head(xdp, 7);
+		break;
+	case 1:
+		/* By default, there is no headroom, so we create some (32),
+		 * assuming test 0 above is successful, then grow back 7 bytes.
+		 */
+		bpf_xdp_set_current_frag(xdp, 1);
+		bpf_xdp_adjust_head(xdp, 32);
+		bpf_xdp_adjust_head(xdp, -7);
+		break;
+	default:
+		return XDP_DROP;
+	}
+
+	bpf_xdp_set_current_frag(xdp, 0);
+	fill_xdp_packet(xdp);
+
+	return XDP_PASS;
+}
+
 
 char _license[] SEC("license") = "GPL";
